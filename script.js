b2338c28ce60c99dcfbc46a4589fc89e9b80bbd8
jaensson <elias.jaensson@gmail.com>
2022-10-07 15:27:52 +0200

    Regular expressions zipcode

--- script.js
+++ script.js
@@ -6,16 +6,56 @@ function init() {
     form = document.getElementById("booking");
     totalElem = document.getElementById("totalCost");
 
-
     form.persons.disabled = true;
     form.persons.parentNode.style.color = "#999";
 
-    form.addEventListener("change", updatePrice)
+    form.addEventListener("change", checkIfFamilyRoom);
+    form.addEventListener("change", updatePrice);
+
+    form.city.style.textTransform = "Uppercase";
+
+    form.zipcode.addEventListener("keyup", e => {
+        console.log(form.zipcode.value);
+        let value = form.zipcode.value;
+        value = parseInt(form.zipcode.value);
+        let valid = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+
+
+        if(valid.test(value)) {
+            form.zipcode.parentNode.parentNode.childNodes[2].innerText = "";
+        } else {
+            form.zipcode.parentNode.parentNode.childNodes[2].innerText = "Du får inte ha några bokstäver eller färre än 5 siffror.";
+        }
+    });
+
+    form.telephone.addEventListener("keyup", e => {
+        console.log(form.telephone.value);
+        let valid = /^0/;
+        let value = form.telephone.value;
+
+        if(valid.test(value)) {
+            form.telephone.parentNode.parentNode.childNodes[2].innerText = "";
+        } else {
+            form.telephone.parentNode.parentNode.childNodes[2].innerText = "Du får inte ha några bokstäver och det måste börja på 0";
+        }
+
+    });
+   
 }
 
-function updatePrice() {
-    let room = getValueFromInput(form.roomType);
-    if(room === 950) {
+/**
+ * Checks if it is a family room
+ */
+function checkIfFamilyRoom() {
+    let room;
+    for(let i = 0; i < form.roomType.length; i++) {
+        if(form.roomType[i].checked) {
+            let value = form.roomType[i].value.split(",");
+            room = value[0];
+        }
+    }
+
+    if(room === "familjerum") {
         form.persons.disabled = false;
         form.persons.parentNode.style.color = "#000";
         form.addition[2].disabled = true;
@@ -26,35 +66,36 @@ function updatePrice() {
         form.addition[2].disabled = false;
         form.addition[2].parentNode.style.color = "#000";
     }
+}
 
+/**
+ * Updates total price
+ */
+function updatePrice() {
+    let room = getPriceValueFromInput(form.roomType);
     let nights = parseInt(form.nights.value);
-    let extra = getValueFromInput(form.addition);
+    let extra = getPriceValueFromInput(form.addition);
     let total = (room + extra) * nights;
 
     totalElem.innerText = total;
 }
 
-
 /**
- * @param {elementList} elementList 
- * @returns element
+ * Get prices from checked inputs
+ * @param {elementList} 
+ * @returns elementValue
  */
-function getValueFromInput(elementList) {
-    let element = 0;
+function getPriceValueFromInput(elementList) {
+    let elementValue = 0;
 
     for(let i = 0; i < elementList.length; i++) {
         if(elementList[i].checked && elementList[i].disabled === false) {
-            console.log("inne");
             let value = elementList[i].value.split(",");
-            if(elementList[i].type === "radio" || element === undefined) {
-                element = parseInt(value[1]);
-            } else {
-                element += parseInt(value[1]);
-            }
+            elementValue += parseInt(value[1]);
         }
     }
 
-    return element;
+    return elementValue;
 }
 
 window.onload = init;
