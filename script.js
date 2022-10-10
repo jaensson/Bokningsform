de9da1fdd40cb1afb31c900199749198139fa125
jaensson <elias.jaensson@gmail.com>
2022-10-10 16:17:39 +0200

    Regular expressions phone/zipcode/campaign

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
-let form;
-let totalElem;
+// Global variables
+let form;               // Reference to the form
+let totalElem;          // Reference to total cost
 
 
+// Initializing global variables
 function init() {
+    // References to element
     form = document.getElementById("booking");
     totalElem = document.getElementById("totalCost");
 
-    form.persons.disabled = true;
-    form.persons.parentNode.style.color = "#999";
-
+    // Event handlers
     form.addEventListener("change", checkIfFamilyRoom);
     form.addEventListener("change", updatePrice);
 
-    form.city.style.textTransform = "Uppercase";
+    // Input for campaign code
+    form.campaigncode.addEventListener("keypress", e => {
+        let value = form.campaigncode.value;                    // Input value campaign code
+        let valid = /[a-zA-Z]{3}-[0-9]{2}-[a-zA-Z][0-9]$/;      // Three letters, -, two digits, -, a letter, a digit
 
-    form.zipcode.addEventListener("keyup", e => {
-        console.log(form.zipcode.value);
-        let value = form.zipcode.value;
-        value = parseInt(form.zipcode.value);
-        let valid = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+        if(valid.test(value)) {                                 // Validates users input
+            form.campaigncode.style.backgroundColor = "green";
+        } else {
+            form.campaigncode.style.backgroundColor = "red";
+        }
+    });
 
+    // Input for zipcode
+    form.zipcode.addEventListener("keypress", e => {
+        let value = form.zipcode.value;                                                 // Input value zipcode
+        value = value.replace(/[^a-zA-Z0-9 ]/g, "").replace(/[a-zåäöA-ZÅÄÖ ]/g, "");    // Replaces all characters except digits
+        form.zipcode.value = value;
+        let valid = /^[1-9][0-9]{4}$/;                                                  // First 1-9, four digits
 
-        if(valid.test(value)) {
+        if(valid.test(value)) {                                                         // Validates users input
             form.zipcode.parentNode.parentNode.childNodes[2].innerText = "";
         } else {
-            form.zipcode.parentNode.parentNode.childNodes[2].innerText = "Du får inte ha några bokstäver eller färre än 5 siffror.";
+            form.zipcode.parentNode.parentNode.childNodes[2].innerText = "Du får endast ha 5 siffor men inte börja på 0";
         }
     });
 
-    form.telephone.addEventListener("keyup", e => {
-        console.log(form.telephone.value);
-        let valid = /^0/;
-        let value = form.telephone.value;
-
-        if(valid.test(value)) {
+    // Input for telephone number
+    form.telephone.addEventListener("keypress", e => {
+        let value = form.telephone.value;                       // Input value telephone
+        value = value.replace(/[a-zåäöA-ZÅÄÖ]/g, "");           // Replaces all characters except digits
+        form.telephone.value = value;
+        let valid = /^0[0-9]{3}[-/ ]?[0-9]{2,7}$/;              // First 0, Three digits, Either (slash, hyphen or space), Two to Seven digits 
+        
+        if(valid.test(value)) {                                 // Validates users input
             form.telephone.parentNode.parentNode.childNodes[2].innerText = "";
+            form.telephone.style.backgroundColor = "green";
         } else {
             form.telephone.parentNode.parentNode.childNodes[2].innerText = "Du får inte ha några bokstäver och det måste börja på 0";
+            form.telephone.style.backgroundColor = "red";
+        }
+    });
+
+    // Input for mail address
+    form.email.addEventListener("keypress", e => {
+        let value = form.email.value;
+        let valid = /^(?!\.)(\w|-|\.){1,64}(?!\.)@(?!\.)[-.a-zåäö0-9]{4,253}$/;
+        let secondValid = /(\W|^)[\w.\-]{0,25}@(yahoo|hotmail|gmail)\.com(\W|$)/;
+
+        if(valid.test(value)) {
+            form.email.parentNode.parentNode.childNodes[1].innerText = "";
+        } else {
+            form.email.parentNode.parentNode.childNodes[1].innerText = "Skriv in en giltig mejladdress";
         }
 
+        console.log(value);
     });
-   
+
+    // Default values for updating input
+    form.city.style.textTransform = "Uppercase";
+    form.persons.disabled = true;
+    form.persons.parentNode.style.color = "#999";
 }
 
 /**
@@ -69,7 +102,7 @@ function checkIfFamilyRoom() {
 }
 
 /**
- * Updates total price
+ * Updates current total price
  */
 function updatePrice() {
     let room = getPriceValueFromInput(form.roomType);
@@ -98,4 +131,5 @@ function getPriceValueFromInput(elementList) {
     return elementValue;
 }
 
+// Runs init when window is loaded
 window.onload = init;
